02bd68f347ef7ec5bd814eae3579f5b4e188bd8b
LiQing <qing.li1@algoblu.com>
2023-05-17 15:06:35 +0800

    version 0.03
    添加了配置文件读取

--- core/main.py
+++ core/main.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from package.log import Log
+from configparser import ConfigParser
 from package.ping_address import ping
 from package.system_load import get_system_load
 from package.interface_traffic import get_net_io_counters
 
 
-def main():
-    
+def sys_load():
     sys_load = get_system_load()
-    print(sys_load)
+    print(sys_load)    
     
-    rx_bytes, tx_bytes = get_net_io_counters('以太网')
+
+def interface_traffic(nic_name):
+    rx_bytes, tx_bytes = get_net_io_counters(nic_name)
     print("以太网: Received {} bytes, Sent {} bytes".format(rx_bytes, tx_bytes))
     
-    packet_loss_rate, avg_rtt = ping('8.8.8.8')
+
+def ping_address(ip_address):
+    packet_loss_rate, avg_rtt = ping(ip_address)
     print(f'丢包率：{packet_loss_rate}, 平均时延：{avg_rtt}ms')
+
+
+def main():
+    
+    cfg = ConfigParser()
+    cfg.read('../conf/config.ini')
+    cfg.sections()
+
+    cfg.get('zabbix conf','Server')
+    cfg.get('zabbix conf','ServerPort')
+    cfg.get('zabbix conf','Hostname')
+    cfg.get('zabbix conf','DiscoveryHours')
+
+    if cfg.getboolean('zabbix item','Item.sys'):
+        sys_load()
+    
+    if cfg.getboolean('zabbix item','Item.if'):
+        interface_traffic('以太网')
+        
+    if cfg.getboolean('zabbix item','Item.ping'):
+        ping_address('114.114.114.114')
     
     log = Log(__name__).getlog()
     log.debug('test.py: main()')
