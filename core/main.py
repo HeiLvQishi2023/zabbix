8047dcfc3b3f56a955a8d5e02cfd806875754fd8
LiQing <qing.li1@algoblu.com>
2023-05-17 15:48:24 +0800

    修复冲突

--- core/main.py
+++ core/main.py
@@ -7,19 +7,46 @@ from package.system_load import get_system_load
 from package.interface_traffic import get_net_io_counters
 
 
+log = Log(__name__).getlog()
+
+
+def sys_load():
+    sys_load = get_system_load()
+    log.info(f' 系统负载：{sys_load}')
+    # print(sys_load)    
+    
+
+def interface_traffic(nic_name):
+    rx_bytes, tx_bytes = get_net_io_counters(nic_name)
+    log.info(f' {nic_name}: Received {rx_bytes} bytes, Sent {tx_bytes} bytes')
+    # print("以太网: Received {} bytes, Sent {} bytes".format(rx_bytes, tx_bytes))
+    
+
+def ping_address(ip_address):
+    packet_loss_rate, avg_rtt = ping(ip_address)
+    log.info(f' 地址: {ip_address} ,丢包率：{packet_loss_rate}, 平均时延：{avg_rtt}ms')
+    # print(f'丢包率：{packet_loss_rate}, 平均时延：{avg_rtt}ms')
+
+
 def main():
     
-    sys_load = get_system_load()
-    print(sys_load)
+    cfg = ConfigParser()
+    cfg.read('./conf/config.ini')
+    cfg.sections()
+
+    cfg.get('zabbix conf','Server')
+    cfg.get('zabbix conf','ServerPort')
+    cfg.get('zabbix conf','Hostname')
+    cfg.get('zabbix conf','DiscoveryHours')
+
+    if cfg.getboolean('zabbix item','Item.sys'):
+        sys_load()
     
-    rx_bytes, tx_bytes = get_net_io_counters('以太网')
-    print("以太网: Received {} bytes, Sent {} bytes".format(rx_bytes, tx_bytes))
+    if cfg.getboolean('zabbix item','Item.if'):
+        interface_traffic('eno1')
+        
+    if cfg.getboolean('zabbix item','Item.ping'):
+        ping_address('8.8.8.6')
     
-    packet_loss_rate, avg_rtt = ping('8.8.8.8')
-    print(f'丢包率：{packet_loss_rate}, 平均时延：{avg_rtt}ms')
-    
-    log = Log(__name__).getlog()
-    log.debug('test.py: main()')
-    log.warning('test.py: main()')
-    
-    
+    log.debug('main.py: main()')
+    log.warning('main.py: main()')
